2bf89bf7be5f11a6a359e657faeeaf52b4c72dff
wilkofm <maxferriswilkinson@gmail.com>
2024-08-07 00:20:07 +1000

    Committing lab exercise 5 to main

---
+++ Module1/Module1/LabExercises/exercise5.js
@@ -0,0 +1,6 @@
+let groups = ["flo", "pinkpantheress", "cubsport", "newjeans", "charlixcx"];
+groups[1] = "yves";
+groups[4] = "troye";
+groups.unshift("xg");
+let lastGroup = groups.pop();
+console.log(groups);
