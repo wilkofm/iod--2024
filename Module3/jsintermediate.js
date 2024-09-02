a81d1021da7bc5be2d3f8c961039d67c1f97c2b5
wilkofm <maxferriswilkinson@gmail.com>
2024-09-02 21:06:21 +1000

    Committing after class

--- Module3/jsintermediate.js
+++ Module3/jsintermediate.js
@@ -1,4 +1,25 @@
-const spliceArray = ["I", "study", "JavaScript", "right", "now"];
-const removed = spliceArray.splice(2, 3, "dance");
-console.log(removed);
-console.log(spliceArray);
+// const spliceArray = ["I", "study", "JavaScript", "right", "now"];
+// const removed = spliceArray.splice(2, 3, "dance");
+// console.log(removed);
+// console.log(spliceArray);
+
+// function displayComponent({ height = 200, width = 100, title }) {
+//   console.log(
+//     `<div style="width:${width}px; height:${height}px"><h2>${title}</h2></div>`
+//   );
+// }
+// displayComponent({ width: 200, title: "My Awesome Component" });
+// displayComponent({ title: "My Amazing Component" });
+// displayComponent({ width: 300, height: 300, title: "My Average Component" });
+
+// //Date and Time
+
+// const now = new Date()
+// console.log( now ) // 2023-03-26T11:45:59.096Z
+// console.log( +now ) // 1679832116638 - number of milliseconds since epoch
+
+const now = new Date();
+console.log(now);
+console.log(now.toLocaleDateString());
+console.log(now.toLocaleTimeString());
+console.log(now.toLocaleString());
