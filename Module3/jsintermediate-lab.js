e48fe567bc5142044f95a7c57fd6370be0e4273e
wilkofm <maxferriswilkinson@gmail.com>
2024-09-04 20:01:21 +1000

    Committing in class unfinished lab work

--- Module3/jsintermediate-lab.js
+++ Module3/jsintermediate-lab.js
@@ -63,20 +63,80 @@ function findMatchingAnimals(beginsWith) {
 
 // 4.
 
-function camelCase(cssProp) {
-  const removeHyphen = cssProp.split("-").join("");
-  return removeHyphen;
-}
-
-console.log(camelCase("margin-left"));
-console.log(camelCase("background-image"));
-console.log(camelCase("display"));
-
-//function needs to Uppercase first letter of each word after dash
-
-//test answer
-
+// Original answer
 // function camelCase(cssProp) {
-//   const addUppercase = cssProp.replace("-" + 1).toUpperCase;
+//   const removeHyphen = cssProp.split("-").join("");
+//   return removeHyphen;
+// }
+
+//Test Answer
+// function camelCase(cssProp) {
+//   const addUppercase = cssProp
+//     .replace("-" + 1)
+//     .charAt(4)
+//     .toUpperCase();
 //   return addUppercase;
 // }
+
+//Practice Answer 1
+// function camelCase(cssProp) {
+//   const removeHyphen = cssProp
+//     .split("-")
+//     .map(camelCase (word) {
+//       return word.charAt(0).toUpperCase() + word.slice(1);
+//     })
+//     .join("");
+// }
+
+function camelCase(cssProp) {
+  let words = cssProp.split("-"); //splits the two words at the dash
+  let camelCaseStr = words[0].toLowerCase(); //first word to lowercase
+  for (let i = 1; i < words.length; i++) {
+    //runs a loop to end after second word
+    camelCaseStr +=
+      words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase(); //starts on second word (words[i]), turns the first letter (.charAt90)) uppercase, then from the second letter onwards (slice 1) changes to lowercase.
+  }
+
+  return camelCaseStr;
+}
+
+// console.log(camelCase("margin-left"));
+// console.log(camelCase("background-image"));
+// console.log(camelCase("display"));
+
+// b) Create variants of the camelCase function that use different types of for loops, and
+// c) with and without the conditional operator.
+
+//5.
+
+let twentyCents = 0.2;
+let tenCents = 0.1;
+// console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`);
+// 0.2 + 0.1 = 0.30000000000000004
+
+let fixedTwenty = twentyCents.toFixed(2);
+let fixedTen = tenCents.toFixed(2);
+// console.log(fixedTwenty + fixedTen); //why is this not working?
+
+// this is giving the answer "0.200.10" because it is concatenating "0.20 and 0.10" as 'two fixed(2)' turns the numbers into a string
+
+function currencyAddition(float1, float2) {
+  let tenCents = float1.toFixed(2);
+  let twentyCents = float2.toFixed(2);
+  let addition = parseFloat(tenCents) + parseFloat(twentyCents);
+  return addition.toFixed(2);
+}
+
+// console.log(currencyAddition(0.1, 0.2));
+// console.log(0.3 == currencyAddition(0.1, 0.2)); // true
+
+function currencyOperation(float1, float2, operation) {
+  let tenCents = float1.toFixed(2);
+  let twentyCents = float2.toFixed(2);
+  switch (operation) {
+    case "+":
+      return tenCents + twentyCents;
+  }
+}
+
+console.log(currencyOperation(0.1, 0.2, "+"));
